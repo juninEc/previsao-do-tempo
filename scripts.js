d151d813d7e3777c66fd31ad60fbfc58206e5467
Eduardo Júnior <junivico20@outlook.com>
2023-07-10 15:46:28 -0300

    feat: atualizações finais.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,33 @@
-const key = "eae8e1c67dbcf3f8db87c4e8a5d4a2b0"
+const key = "eae8e1c67dbcf3f8db87c4e8a5d4a2b0";
 
+function dataOnScreen(data) {
+  document.querySelector(".city").innerHTML = "Tempo em " + data.name;
+  document.querySelector(".graus").innerHTML =
+    Math.floor(data.main.temp) + "°C";
+  document.querySelector(".clima").innerHTML = data.weather[0].description;
+  document.querySelector(".umidade").innerHTML =
+    "Umidade: " + data.main.humidity + "%";
+  document.querySelector(
+    ".img-icon"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 
-function dataOnScreen(data){
-    document.querySelector(".city").innerHTML = "Tempo em " + data.name
-    document.querySelector(".graus").innerHTML = Math.floor(data.main.temp) + "°C"
-    document.querySelector(".clima").innerHTML = data.weather[0].description
-    document.querySelector(".umidade").innerHTML = "Umidade: " + data.main.humidity + "%"
-    document.querySelector(".img-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-
-    console.log(data)
+  console.log(data);
 }
-async function citySearch(city){
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+async function citySearch(city) {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+  ).then((response) => response.json());
 
-    dataOnScreen(data)
+  dataOnScreen(data);
 }
 
-function clickSearch(){
-    const city = document.querySelector(".input-city").value
-    citySearch(city)
+function clickSearch() {
+  const city = document.querySelector(".input-city").value;
+  citySearch(city);
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString("pt-BR");
+  document.querySelector(".data").innerHTML = formattedDate;
+});
